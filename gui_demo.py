608f50e047b4d6b6028e5ae528630ea6f31f305c
simondlevy <simon.d.levy@gmail.com>
2018-06-28 22:35:06 -0400

    Switched to MIT License

--- gui_demo.py
+++ gui_demo.py
@@ -3,21 +3,8 @@ Display Nengo PD controller in Nengo GUI
 
 Copyright 2018 Simon D. Levy
 
-This file is part of NengoCPlusPlus.
-
-NengoCPlusPlus is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of the 
-License, or (at your option) any later version.
-This code is distributed in the hope that it will be useful,     
-but WITHOUT ANY WARRANTY without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License 
-along with this code.  If not, see <http:#www.gnu.org/licenses/>.
+MIT License
 '''
-
 from nengo_pidcontrol import PIDController
 
 controller = PIDController(.1, .1, in_gui=True)
--- nengo_pidcontrol.py
+++ nengo_pidcontrol.py
@@ -4,19 +4,7 @@ Nengo PID control
 
 Copyright 2018 Terry Stewart, Simon D. Levy, Melanie Jouaiti
 
-This file is part of NengoCPlusPlus.
-
-NengoCPlusPlus is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as 
-published by the Free Software Foundation, either version 3 of the 
-License, or (at your option) any later version.
-This code is distributed in the hope that it will be useful,     
-but WITHOUT ANY WARRANTY without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public License 
-along with this code.  If not, see <http:#www.gnu.org/licenses/>.
+MIT License
 '''
 
 import nengo
